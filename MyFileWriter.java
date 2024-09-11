9d5bc134a26acb27bfb8f70a3ca3f9ebd39678a0
Jacob Massey <jmassey1@hwemail.com>
2024-09-11 00:43:32 -0700

    filesize + secrets done and working

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -1,4 +1,24 @@
-touch .kevinsfeet.txt
-mkdir .footFolder
-cd .footFolder
-touch mariahsfeet.date
+import java.io.*; 
+public class MyFileWriter{
+    public static void main (String [] args)
+    {
+        new File(".secretFolder").mkdirs();
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(".secretFolder/thePassword.dat"))) {
+            bufferedWriter.write("kevinsFeet");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        printFileSize(".secretFolder/thePassword.dat");
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(".mariahsInfo.txt"))) {
+            bufferedWriter.write("banana123");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        printFileSize(".mariahsInfo.txt");
+    }
+
+    public static void printFileSize(String file)
+    {
+        System.out.println(file.length());
+    }
+}
