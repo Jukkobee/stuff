9b810ffe93e50b7a3ba121dcc9af46c262ed8b05
Kyara Zhou <kzhou1@hwemail.com>
2024-09-12 18:33:25 -0700

    tester method
    forgot about this

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -21,15 +21,17 @@ public class MyFileWriter{
     {
         System.out.println(file.length());
     }
-    private static void printTotalFileSize(String... fileNames) {
+    
+    public static void printTotalFileSize(String... fileNames) {
         long totalSize = 0;
         for (String fileName : fileNames) {
             File file = new File(fileName);
             if (file.exists()) {
-                totalSize += file.length();
+            totalSize += file.length();
             }
         }
         System.out.println("Total size of all files: " + totalSize + " bytes");
     }
+
     
 }
